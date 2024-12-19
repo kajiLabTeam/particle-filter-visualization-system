3c1b46b3583e4cf1a9fd58291001fd0ed11c7ad1
nanairo2743 <136669608+nanairo2743@users.noreply.github.com>
2024-12-19 21:55:33 +0900

    fix: error.pyにcontrollerをimportして標準偏差の部分をcontrollerで取得した部分に変更

--- controller/perform_particlefiltering_controller.py
+++ controller/perform_particlefiltering_controller.py
@@ -3,6 +3,8 @@ from pydantic import BaseModel
 
 # モジュールレベルの変数として particle_count を定義
 particle_count = 1000 # 初期値
+particle_step_error_sd = 10
+particle_angle_error_sd = 10
 
 class PerformParticleFilteringRequest(BaseModel):
     """
@@ -27,12 +29,38 @@ def get_particle_count() -> int:
     """
     return particle_count
 
+def get_particle_step_error_sd() -> int:
+    """
+    現在の particle_step_error_sd を取得する関数
+    """
+    return particle_step_error_sd
+
+def get_particle_angle_error_sd() -> int:
+    """
+    現在の particle_angle_error_sd を取得する関数
+    """
+    return particle_angle_error_sd
+
 def set_particle_count(value: int):
     """
     particle_count を更新する関数
     """
     global particle_count
     particle_count = value
+
+def set_particle_step_error_sd(value: int):
+    """
+    particle_step_error_sd を更新する関数
+    """
+    global particle_step_error_sd
+    particle_step_error_sd = value
+
+def set_particle_angle_error_sd(value: int):
+    """
+    particle_angle_error_sd を更新する関数
+    """
+    global particle_angle_error_sd
+    particle_angle_error_sd = value
 
 async def perform_particlefiltering(
     request: PerformParticleFilteringRequest
@@ -42,7 +70,6 @@ async def perform_particlefiltering(
     """
     # Use the request data to perform particle filtering
     # For now, we'll just return a dummy response
-    set_particle_count(1010)
     return {
     "message": "Particle filtering performed successfully",
     "particle_count": 1010,
